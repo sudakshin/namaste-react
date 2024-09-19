91e61cd3e7b9e23bcf7e6e90c14c5aaf3cf4bc67
Sudakshin <sudakshin9@gmail.com>
2024-09-20 02:35:46 +0530

    added filter reset

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,6 @@ import RestaurantCard from './RestaurantCard';
 import resObj from '../utils/restaurant.json';
 
 const Body = () => {
-	// State to hold the filtered restaurant data
 	const [filteredRestaurants, setFilteredRestaurants] = useState(resObj);
 
 	return (
@@ -12,19 +11,26 @@ const Body = () => {
 				<button
 					className='filter-btn'
 					onClick={() => {
-						// Filter restaurants with avgRating > 4.5
 						const topRatedRestaurants = resObj.filter(
 							(res) => res.info.avgRating > 4.5
 						);
-						console.log(topRatedRestaurants);
-
-						// Update the state with the filtered restaurants
 						setFilteredRestaurants(topRatedRestaurants);
 					}}
 				>
 					Top Rated Restaurants
 				</button>
+
+				<button
+					className='filter-btn'
+					onClick={() => {
+						console.log('Resetting filter');
+						setFilteredRestaurants(resObj);
+					}}
+				>
+					Reset Filter
+				</button>
 			</div>
+
 			<div className='res-container'>
 				{filteredRestaurants.map((restaurant) => (
 					<RestaurantCard
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,5 +1,4 @@
 import { CDN_URL } from '../utils/constants';
-console.log(CDN_URL);
 
 const RestaurantCard = (props) => {
 	const { resData } = props;
