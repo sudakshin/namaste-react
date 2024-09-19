8cd41566894597cdbb58da1fa104d92c054e5939
Sudakshin <sudakshin9@gmail.com>
2024-09-20 02:14:46 +0530

    added filter

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 import resObj from '../utils/restaurant.json';
 
 const Body = () => {
+	// State to hold the filtered restaurant data
+	const [filteredRestaurants, setFilteredRestaurants] = useState(resObj);
+
 	return (
 		<div className='body'>
-			<div className='search'>Search</div>
+			<div className='filter'>
+				<button
+					className='filter-btn'
+					onClick={() => {
+						// Filter restaurants with avgRating > 4.5
+						const topRatedRestaurants = resObj.filter(
+							(res) => res.info.avgRating > 4.5
+						);
+						console.log(topRatedRestaurants);
+
+						// Update the state with the filtered restaurants
+						setFilteredRestaurants(topRatedRestaurants);
+					}}
+				>
+					Top Rated Restaurants
+				</button>
+			</div>
 			<div className='res-container'>
-				{resObj.map((restaurant) => (
+				{filteredRestaurants.map((restaurant) => (
 					<RestaurantCard
 						key={restaurant.info.id}
 						resData={restaurant}
