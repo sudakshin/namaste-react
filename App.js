5d1dc4406afb0def054de16e2418c52069ede200
Sudakshin <sudakshin9@gmail.com>
2024-09-19 22:40:29 +0530

    cards and css + props

--- App.js
+++ App.js
@@ -1,37 +1,83 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement(
-	'div',
-	{},
-	'Hello cewitgeioyhowhyierhgeioubre'
-);
-
-const jsxHeading = (
-	<>
-		<h1
-			id='heading'
-			className='head'
-		>
-			hello world
-		</h1>
-		<h2>hello</h2>
-	</>
-);
-
-const Title = () => {
-	return <h1>Title</h1>;
+const Header = () => {
+	return (
+		<div className='header'>
+			<div className='logo-container'>
+				<img
+					className='logo'
+					src='https://cdn.dribbble.com/users/1635051/screenshots/4291569/media/f1dc908d5e3aa58ae7beed58c623acb6.png?resize=800x600&vertical=center'
+					alt='Food App Logo'
+				/>
+			</div>
+			<div className='nav-items'>
+				<ul>
+					<li>Home</li>
+					<li>About Us</li>
+					<li>Contact Us</li>
+					<li>Cart</li>
+				</ul>
+			</div>
+		</div>
+	);
 };
 
-const HeadingComponent = () => (
-	<>
-		<Title />
-		<h1>Namaste react {Math.floor(Math.random() * 100)}</h1>
-		<h1>Namaste react {jsxHeading}</h1>
-	</>
-);
+const RestaurantCard = (props) => {
+	console.log(props);
+
+	const { resName, cuisine, rating, delTime } = props;
+	return (
+		<div
+			className='res-card'
+			style={{ backgroundColor: '#f0f0f0' }}
+		>
+			<img
+				src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/jiwjgh4rhaobq5ofgrly'
+				alt='Grameen Kulfi'
+				className='res-logo'
+			/>
+			<div className='info'>
+				<span className='name'>{resName}</span>
+				<span className='cuisine'>{cuisine}</span>
+				<span className='rating'>{rating}</span>
+				<span className='del-time'>{delTime}</span>
+			</div>
+		</div>
+	);
+};
+
+const Body = () => {
+	return (
+		<div className='body'>
+			<div className='search'>Search</div>
+			<div className='res-container'>
+				<RestaurantCard
+					resName='Grameen Kulfi'
+					cuisine='Desserts, Ice Cream'
+					rating='4.7'
+					delTime='25-30 mins'
+				/>
+				<RestaurantCard
+					resName='NIC Ice Creams'
+					cuisine='Ice Cream, Desserts'
+					rating='4.6'
+					delTime='35-40 mins'
+				/>
+			</div>
+		</div>
+	);
+};
+
+const AppLayout = () => {
+	return (
+		<div className='app'>
+			<Header />
+			<Body />
+		</div>
+	);
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root.render(jsxHeading);
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
